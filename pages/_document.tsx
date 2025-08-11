e2ff74e4120a52e54e33f8da01a7970fbc165717
Henry <henrywithu@gmail.com>
2025-08-11 22:43:07 +0800

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,19 +8,19 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="Feel free, be cool."
           />
-          <meta property="og:site_name" content="nextjsconf-pics.vercel.app" />
+          <meta property="og:site_name" content="Trapnest" />
           <meta
             property="og:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="Feel free, be cool."
           />
-          <meta property="og:title" content="Next.js Conf 2022 Pictures" />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Next.js Conf 2022 Pictures" />
+          <meta property="og:title" content="Trapnest" />
+          <meta name="twitter:card" content="Trapnest" />
+          <meta name="twitter:title" content="Trapnest" />
           <meta
             name="twitter:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="Feel free, be cool."
           />
         </Head>
         <body className="bg-black antialiased">
