847bae5b703d647ed91b5660061ea2bb8996aaef
henrywithu <henrywithu@gmail.com>
2025-08-11 15:18:27 +0000

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -66,7 +66,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
             </p>
             <a
               className="pointer z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4"
-              href="https://cloudinary.henrywithu.com"
+              href="https://henrywithu.com"
               target="_blank"
               rel="noreferrer"
             >
